232b184b24106a2a60f9742a9246e9ee0a95b629
Zin Nyein Htet <T916696@telenor.com.mm>
2020-01-28 13:31:37 +0630

    notebookcapstone

---
+++ notebook_for_capstone_z.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This notebook will be mainly used for the capstone project.
+
+# In[4]:
+
+
+import pandas as pd
+
+
+# In[6]:
+
+
+import numpy as np
+
+
+# In[7]:
+
+
+print("Hello Capstone Project Course!")
+
